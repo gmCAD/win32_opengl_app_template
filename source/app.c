f667dca00b5dde5079d5f58df2418f1bbbf366f8
ryanfleury <ryan.j.fleury@gmail.com>
2020-05-14 23:50:02 -0600

    Add core state

--- source/app.c
+++ source/app.c
@@ -3,15 +3,27 @@
 #include "opengl.h"
 #include "language_layer.c"
 
+typedef struct Core Core;
+struct Core
+{
+    MemoryArena *permanent_arena;
+    MemoryArena *frame_arena;
+};
+global Core *core = 0;
+
 AppEntryPoint_PermanentLoad
 {
     platform = platform_;
     LoadAllOpenGLProcedures();
+    core = MemoryArenaAllocateAndZero(&platform->permanent_arena, sizeof(*core));
+    core->permanent_arena = &platform->permanent_arena;
+    core->frame_arena = &platform->scratch_arena;
 }
 
 AppEntryPoint_HotLoad
 {
     platform = platform_;
+    core = platform->permanent_arena.memory;
 }
 
 AppEntryPoint_HotUnload {}
